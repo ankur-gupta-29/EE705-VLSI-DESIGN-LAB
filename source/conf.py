c98901a91486417479460ee75afc69cdd64295e0
Ankur Gupta <ankur2115gupta29@gmail.com>
2025-01-10 20:38:38 +0530

    backup 2025-01-10 20:38:38

--- source/conf.py
+++ source/conf.py
@@ -2,7 +2,7 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
+import sphinxcontrib.youtube
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
